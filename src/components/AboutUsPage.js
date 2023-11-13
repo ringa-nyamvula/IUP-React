eca17b2f46e4ad4ec70d31e054be9cd409db7967
ringa-nyamvula <nyamvularinga@gmail.com>
2023-11-13 16:27:15 +0300

    Styling

--- src/components/AboutUsPage.js
+++ src/components/AboutUsPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function AboutUsPage() {
   return (
-    <div style={{ backgroundColor: '#FFCC00', padding: '20px' }}>
+    <div style={{ backgroundColor: '#FFCC00', padding: '36px' }}>
       <h1 style={{ color: 'navy' }}>About Us</h1>
       {/* Your navigation bar here */}
       <h2 style={{ color: 'navy' }}>Our Leadership</h2>
