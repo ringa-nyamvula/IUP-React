4246913b1a6e8620171bb8d369be9268d0018898
ringa-nyamvula <nyamvularinga@gmail.com>
2023-11-30 01:45:20 +0300

    Final

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,7 @@
 // src/components/LandingPage.js
-import React, { useState } from 'react';
+import React, { useState } from "react";
 //import { Link } from 'react-router-dom';
-import RegistrationForm from './RegistrationForm'; // Import the RegistrationForm
+import RegistrationForm from "./RegistrationForm"; // Import the RegistrationForm
 
 function LandingPage() {
   const [showForm, setShowForm] = useState(false);
@@ -13,26 +13,42 @@ function LandingPage() {
   };
 
   return (
-    <div style={{ textAlign: 'center', paddingTop: '100px' }}>
-      <h1 style={{ color: 'blue' }}>IMARISHA UCHUMI PARTY (UIP)</h1>
-      <p style={{ color: 'blue' }}>Power Line On post</p>
+    <div
+      style={{
+        textAlign: "center",
+        paddingTop: "200px",
+        paddingBottom: "400px",
+        backgroundImage: 'url("/images/imarisha.png")',
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <h1 style={{ color: "blue", fontSize: "48px" }}>
+        IMARISHA UCHUMI PARTY (UIP)
+      </h1>
+      <p style={{ color: "blue", fontSize: "36px" }}>Power Line On post</p>
+      <br />
       <button
-        style={{ backgroundColor: 'yellow' }}
+        style={{
+          backgroundColor: "yellow",
+          padding: "30px",
+          borderRadius: "5px",
+        }}
         onClick={() => setShowForm(true)}
       >
-        Register as a User
+        REGISTER NOW AS A MEMBER
       </button>
 
-      {showForm && (
-        <RegistrationForm onRegister={handleRegister} />
-      )}
+      {showForm && <RegistrationForm onRegister={handleRegister} />}
 
       {registeredUsers.length > 0 && (
         <div>
           <h2>Registered Users:</h2>
           <ul>
             {registeredUsers.map((user, index) => (
-              <li key={index}>{`${user.name} - ${user.idOrPassport}, ${user.phoneNumber}, ${user.email}`}</li>
+              <li
+                key={index}
+              >{`${user.name} - ${user.idOrPassport}, ${user.phoneNumber}, ${user.email}`}</li>
             ))}
           </ul>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,96 @@
 // src/components/Navbar.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/landing">Landing Page</Link></li>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/manifesto">Manifesto</Link></li>
-        <li><Link to="/strategies">Strategies</Link></li>
-        <li><Link to="/implementation">Implementation</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-    </nav>
+    <div
+      className="container-fluid"
+      style={{
+        textAlign: "right",
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+      }}
+    >
+      <nav
+        className="navbar navbar-expand-lg  navbar-light bg-light"
+        style={{
+          marginTop: "10px",
+          padding: "15px 10px",
+          textAlign: "right",
+        }}
+      >
+        <div
+          className="collapse navbar-collapse"
+          id="navbarNav"
+          style={{ textAlign: "right" }}
+        >
+          <ul
+            className="navbar-nav "
+            style={{ display: "flex", justifyContent: "flex-end" }}
+          >
+            <li
+              className="nav-item"
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Link to="/landing" className="nav-link">
+                Landing Page
+              </Link>
+            </li>
+            <li
+              className="nav-item"
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Link to="/home" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li
+              className="nav-item"
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Link to="/about" className="nav-link">
+                About Us
+              </Link>
+            </li>
+            <li
+              className="nav-item"
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Link to="/manifesto" className="nav-link">
+                Manifesto
+              </Link>
+            </li>
+            <li
+              className="nav-item"
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Link to="/strategies" className="nav-link">
+                Strategies
+              </Link>
+            </li>
+            <li
+              className="nav-item"
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Link to="/implementation" className="nav-link">
+                Implementation
+              </Link>
+            </li>
+            <li
+              className="nav-item"
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Link to="/contact" className="nav-link">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 }
 
